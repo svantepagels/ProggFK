a194234bb1f78eca536398165a9a6e03df20f1b4
Svante <svantepagels@gmail.com>
2016-01-27 21:43:38 +0100

    Uppgift 1-3 klara

---
+++ Documents/Skola/Progg FK/edaa01-workspace/lab2/src/TestFifoQueueAppend.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import queue.FifoQueue;
+
+public class TestFifoQueueAppend<E> {
+	private FifoQueue<Integer> a;
+	private FifoQueue<Integer> b;
+
+	@Before
+	public void setUp() throws Exception {
+		a = new FifoQueue<Integer>();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		a.last = null;
+		
+	}
+
+	@Test
+	public void twoEmptyQueues() {
+		a.append(b);
+		assertTrue("Queue size not 0", a.size()==0);
+		assertTrue("Queue not empty", a.last==null);
+	}
+	
+	@Test
+	public void appendEmptyQueue() {
+		int[] c = {1, 2, 3, 4, 5, 6, 7};
+		for(int i : c){
+			a.offer(i);
+		}
+		a.append(b);
+		assertTrue("Queue size not 7", a.size()==7);
+		assertTrue("Last element not 7", a.peek()==7);
+	}
+	@Test
+	public void appendToEmptyQueue() {
+		int[] c = {8, 9, 10, 11, 12, 13, 14};
+		for(int i : c){
+			a.offer(i);
+		}
+		b.append(a);
+		assertTrue("Queue size not 7", a.size()==7);
+		assertTrue("Last element not 7", a.peek()==14);
+	}
+	@Test
+	public void appendTwoQueues(){
+		int[] c = {1, 2, 3, 4, 5, 6, 7};
+		for(int i : c){
+			a.offer(i);
+		}
+		int[] d = {8, 9, 10, 11, 12, 13, 14};
+		for(int i : d){
+			b.offer(i);
+		}	
+		b.append(a);
+		assertTrue("Queue size not 14", a.size()==14);
+		assertTrue("Last element not 7", a.peek()==7);
+	}
+}
--- Documents/Skola/Progg FK/edaa01-workspace/lab2/src/queue/FifoQueue.java
+++ Documents/Skola/Progg FK/edaa01-workspace/lab2/src/queue/FifoQueue.java
@@ -1,61 +1,111 @@
 package queue;
+
 import java.util.*;
 
 public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
-	private QueueNode<E> last;
+	public QueueNode<E> last;
 	private int size;
 
 	public FifoQueue() {
 
 	}
 
-	/**	
+	/**
 	 * Returns an iterator over the elements in this queue
+	 * 
 	 * @return an iterator over the elements in this queue
-	 */	
+	 */
 	public Iterator<E> iterator() {
-		return null;
+		return new QueueIterator();
 	}
 
-	/**	
+	private class QueueIterator implements Iterator<E> {
+		private QueueNode<E> pos;
+
+		private QueueIterator() {
+			pos = last;
+		}
+
+		@Override
+		public boolean hasNext() {
+
+			return (pos != null);
+		}
+
+		@Override
+		public E next() {
+
+			return pos.next.element;
+		}
+
+	}
+
+	/**
 	 * Returns the number of elements in this queue
+	 * 
 	 * @return the number of elements in this queue
 	 */
-	public int size() {		
-		return 0;
+	public int size() {
+		return size;
 	}
 
-	/**	
-	 * Inserts the specified element into this queue, if possible
-	 * post:	The specified element is added to the rear of this queue
-	 * @param	x the element to insert
-	 * @return	true if it was possible to add the element 
-	 * 			to this queue, else false
+	/**
+	 * Inserts the specified element into this queue, if possible post: The
+	 * specified element is added to the rear of this queue
+	 * 
+	 * @param x
+	 *            the element to insert
+	 * @return true if it was possible to add the element to this queue, else
+	 *         false
 	 */
 	public boolean offer(E x) {
+		QueueNode<E> temp;
+		if (isEmpty()) {
+			temp = new QueueNode<E>(x);
+			last = temp;
+			last.next = temp;
+		} else {
+			temp = new QueueNode<E>(x);
+			temp.next = last.next;
+			last = temp;
+		}
 		return true;
 	}
 
-	/**	
-	 * Retrieves and removes the head of this queue, 
-	 * or null if this queue is empty.
-	 * post:	the head of the queue is removed if it was not empty
-	 * @return 	the head of this queue, or null if the queue is empty 
+	/**
+	 * Retrieves and removes the head of this queue, or null if this queue is
+	 * empty. post: the head of the queue is removed if it was not empty
+	 * 
+	 * @return the head of this queue, or null if the queue is empty
 	 */
 	public E poll() {
-		return null;
+		if (isEmpty()) {
+			return null;
+		}
+		E temp = last.element;
+		last = last.next;
+		return temp;
 	}
 
-	/**	
-	 * Retrieves, but does not remove, the head of this queue, 
-	 * returning null if this queue is empty
-	 * @return 	the head element of this queue, or null 
-	 * 			if this queue is empty
+	/**
+	 * Retrieves, but does not remove, the head of this queue, returning null if
+	 * this queue is empty
+	 * 
+	 * @return the head element of this queue, or null if this queue is empty
 	 */
 	public E peek() {
-		return null;
+		if (isEmpty()) {
+			return null;
+		}
+		return last.element;
 	}
-
+	
+	//Appends the queue q to the current queue. q is positioned behind the current queue.
+	
+	public void append(FifoQueue<E> q){
+		last.next = q.last.next;
+		last = q.last;
+	}
 
 	private static class QueueNode<E> {
 		E element;
